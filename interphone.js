3f7ab62e3dc2d2aa7279e4044313b8057f4159bc
Eduardo Malpeli <tchatcho66@hotmail.com>
2014-05-12 17:16:54 -0300

    indexOf for IE8

--- interphone.js
+++ interphone.js
@@ -7,6 +7,29 @@
  */
 
 
+/**
+ * IE8 compatibility. indexOf for Array is not present
+ */
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(elt /*, from*/) {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++) {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+};
+
 if (!window.extend) {
   /**
    * window.extend() will join Objects
