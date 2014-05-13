c236bebc760c76b5c6bf2978b36eb8dae7f2ee28
Eduardo Malpeli <tchatcho66@hotmail.com>
2014-05-13 18:20:23 -0300

    Testing JSON parsing

--- interphone.js
+++ interphone.js
@@ -282,7 +282,6 @@ interphone.prototype.onMessage = function (event,_self) {
   var data = (event.data || "");
   var uuid = data.split("--")[0];
   if (uuid != _self.pair) return;
-  console.log(data.split("--")[1])
   // var blob = data.split("--")[1].cypher(_self.uuid+_self.pair);
   var blob = data.split("--")[1]
   console.log(blob)
